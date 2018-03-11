cde9af3244c3f5389d138c6ca3c7ab45c3f8c881
George Lewis <ghl227@gmail.com>
2018-03-11 15:49:32 -0400

    Update server notation

--- src/server.cxx
+++ src/server.cxx
@@ -1,13 +1,38 @@
 
 #include "crow.h"
 
-int main()
-{
+int main() {
+
     crow::SimpleApp app;
 
-    CROW_ROUTE(app, "/")([](){
-        return "Hello world";
-    });
+    app.route_dynamic("/")
+            ([]() {
+                return "Hello world";
+            });
+
+    app.route_dynamic("/hello/<int>")
+            ([](int count) {
+                if (count > 100) {
+                    return crow::response(400);
+                } else {
+                    std::ostringstream os;
+                    os << count << " bottles of beer!";
+                    return crow::response(os.str());
+                }
+            });
+
+    app.route_dynamic("/add_json").methods("POST"_method)
+            ([](const crow::request &req) {
+                auto x = crow::json::load(req.body);
+                if (!x) {
+                    return crow::response(400);
+                } else {
+                    int64_t sum = x["a"].i() + x["b"].i();
+                    std::ostringstream os;
+                    os << sum;
+                    return crow::response{os.str()};
+                }
+            });
 
     app.port(18080).multithreaded().run();
 }
