99ddd15672ec5e7c981c791ddb4a99b3ce2be2ce
George Lewis <ghl227@gmail.com>
2018-04-20 12:13:59 -0400

    Updating for transition

---
+++ test/test_payload.cxx
@@ -0,0 +1,28 @@
+
+
+#include <utility>
+#include <cstdlib>
+#include <string>
+
+#include "gtest/gtest.h"
+#include <database.h>
+#include <payload.h>
+#include <glog/logging.h>
+
+#include "utils.h"
+
+#include "json.h"
+using json = nlohmann::json;
+
+
+TEST(test_payload, creation)
+{
+
+    auto payload = Payload::fromString("Foobar");
+
+    EXPECT_EQ("Foobar", payload.toString());
+
+}
+
+
+
